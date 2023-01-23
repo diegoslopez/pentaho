e47064db6f6f000d7c7e56db3fd9c2e403a99fe6
diegoslopez <diegoslopez@gmail.com>
2023-01-23 12:53:35 -0300

    Archivos ktr - kjb - bat
    Modificaciones a las rutas de acceso para que funcione el .BAT

---
+++ challenge.bat
@@ -0,0 +1,3 @@
+cd "C:\Pentaho\pdi-version-9.4\data-integration"
+call Kitchen.bat /file:D:\Documents\GitHub\pentaho\Trabajo1.kjb "-param:FECHA_INGRESADA=2011-05-17" -logfile=D:\Documents\GitHub\pentaho\logKettle.txt
+exit
