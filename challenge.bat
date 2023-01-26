38b3fb1abaa2c920c950e55de622857ea4383a71
diegoslopez <diegoslopez@gmail.com>
2023-01-26 09:30:53 -0300

    Actualización con correcciones
    Subiendo actualización de archivos, según correcciones hechas.

--- challenge.bat
+++ challenge.bat
@@ -1,3 +1,3 @@
 cd "C:\Pentaho\pdi-version-9.4\data-integration"
-call Kitchen.bat /file:D:\Documents\GitHub\pentaho\Trabajo1.kjb "-param:FECHA_INGRESADA=2011-05-17" -logfile=D:\Documents\GitHub\pentaho\logKettle.txt
+call Kitchen.bat /file:D:\Documents\GitHub\pentaho\Trabajo1.kjb "-param:FECHA_INGRESADA=2011-05-17" "-param:PATH=D:/Documents/GitHub/pentaho/" -logfile=D:\Documents\GitHub\pentaho\logKettle.txt
 exit
